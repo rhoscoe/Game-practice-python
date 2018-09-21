46525d9d46648b4388d0b5adb3c75be672f2d032
rhoscoe <rosieh94@gmail.com>
2018-09-21 20:49:13 +0100

    Update practice_python_9.py

--- practice_python_9.py
+++ practice_python_9.py
@@ -27,20 +27,28 @@ else:
 
 count = 0
 comp = random.randint(1,9)
-print comp
+#print comp - for checking purposes
 
 while True:
     player = (raw_input("Make a guess between 1 and 9, or type 'exit' to leave the game: "))
     if player == "exit":
-        print("You made "+str(count)+" guesses.")
-        break
+        print("You made "+str(count)+" guesses.") #should fix grammar if 1 guess using an if statement
+        break #break the loop and finish the game
     if int(player) > comp:
         print "Too big. Try again."
-        count += 1
+        count += 1 #add to count
+        print("You made "+str(count)+" guesses.")#print count
     elif int(player) < comp:
         print "Too small. Try again."
         count += 1
+        print("You made "+str(count)+" guesses.")
     else:
         print "You did it!"
         count += 1
-        break
+        print("You made "+str(count)+" guesses.")
+        playagain = (raw_input("Would you like to play again? Type 'yes', otherwise type 'exit' to leave the game: "))
+        if playagain.lower() == "yes":
+            count = 0 #reset count
+            comp = random.randint(1,9) #get new random number and stay in the loop
+        else:
+            break #break the loop and finish the game
